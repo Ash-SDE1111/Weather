b4f78bcacca6e71f7c0018d4bab8b629046b2218
Ash-SDE1111 <saxenaashwarya1111@gmail.com>
2023-03-18 22:31:29 -0700

    https issue resolved

--- src/component/weather/temp.js
+++ src/component/weather/temp.js
@@ -8,7 +8,7 @@ const Temp = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q={searchValue}&appid=4ece0e4b44fbf71c7a20286561299ae7`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q={searchValue}&appid=4ece0e4b44fbf71c7a20286561299ae7`;
 
       let res = await fetch(url);
       let data = await res.json();
